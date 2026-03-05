c33af0934a27da9710319ec65aff4539c33cf24f
Daniel <danigar.gb@icloud.com>
2026-03-05 16:14:35 -0600

    Coomint 2

--- src/data/products.js
+++ src/data/products.js
@@ -17,21 +17,21 @@ import gameMortalKombat from '../assets/productos_img/Game_MortalKombat.jpeg';
 import gameValorant from '../assets/productos_img/Game_Valorant.jpeg';
 
 export const todosLosProductos = [
-  { id: 1, name: 'FC 26', image: gameFC26, price: 1199.80 },
-  { id: 2, name: 'FORTNITE', image: gameFornite, price: 399.80 },
-  { id: 3, name: 'FREE FIRE', image: gameFreeFire, price: 199.80 },
-  { id: 4, name: 'SUPER MARIO PARTY JAMBOREE', image: gameMario, price: 999.80 },
-  { id: 5, name: 'ROBLOX', image: gameRoblox, price: 499.80 },
-  { id: 6, name: 'STUMBLE GUY', image: gameStumble, price: 299.80 },
-  { id: 7, name: 'ANIMAL CROSSING', image: gameAnimalCrossing, price: 1099.80 },
-  { id: 8, name: 'BLOOD STRIKE', image: gameBloodStrike, price: 599.80 },
-  { id: 9, name: 'CALL OF DUTY: WARZONE', image: gameCallOfDuty, price: 1399.80 },
-  { id: 10, name: 'CRASH BANDICOOT 4', image: gameCrash, price: 799.80 },
-  { id: 11, name: 'GRANT THEFT AUTO: SAN ANDREAS', image: gameGTA, price: 1199.80 },
-  { id: 12, name: 'GOD OF WAR', image: gameGodOfWar, price: 999.80 },
-  { id: 13, name: 'LAST OF US', image: gameLastOfUs, price: 1199.80 },
-  { id: 14, name: 'MARVEL RIVALS', image: gameMavelRivals, price: 799.80 },
-  { id: 15, name: 'MINECRAFT', image: gameMinecraft, price: 539.80 },
-  { id: 16, name: 'MORTAL KOMBAT 1', image: gameMortalKombat, price: 999.80 },
-  { id: 17, name: 'VALORANT', image: gameValorant, price: 399.80 }
+  { id: 1, name: 'FC 26', image: gameFC26, price: 1199.99 },
+  { id: 2, name: 'FORTNITE', image: gameFornite, price: 399.99 },
+  { id: 3, name: 'FREE FIRE', image: gameFreeFire, price: 199.99},
+  { id: 4, name: 'SUPER MARIO PARTY JAMBOREE', image: gameMario, price: 999.99 },
+  { id: 5, name: 'ROBLOX', image: gameRoblox, price: 99.99 },
+  { id: 6, name: 'STUMBLE GUY', image: gameStumble, price: 299.99 },
+  { id: 7, name: 'ANIMAL CROSSING', image: gameAnimalCrossing, price: 1099.99 },
+  { id: 8, name: 'BLOOD STRIKE', image: gameBloodStrike, price: 199.99 },
+  { id: 9, name: 'CALL OF DUTY: WARZONE', image: gameCallOfDuty, price: 1399.99 },
+  { id: 10, name: 'CRASH BANDICOOT 4', image: gameCrash, price: 799.99 },
+  { id: 11, name: 'GRANT THEFT AUTO: SAN ANDREAS', image: gameGTA, price: 1199.99 },
+  { id: 12, name: 'GOD OF WAR', image: gameGodOfWar, price: 999.99 },
+  { id: 13, name: 'LAST OF US', image: gameLastOfUs, price: 1199.99 },
+  { id: 14, name: 'MARVEL RIVALS', image: gameMavelRivals, price: 799.99 },
+  { id: 15, name: 'MINECRAFT', image: gameMinecraft, price: 499.99 },
+  { id: 16, name: 'MORTAL KOMBAT 1', image: gameMortalKombat, price: 999.99 },
+  { id: 17, name: 'VALORANT', image: gameValorant, price: 199.99 }
 ];
